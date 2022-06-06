5cf999df9cfac2f40fa1429f6df36b34f9f0b8e7
Dener <denerbarretocosta@gmail.com>
2022-06-06 11:59:12 -0400

    Add Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,48 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo_pokemon.svg";
 import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header-bg">
+      <div className="container header">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={(nav) => (nav.isActive ? "nav-active" : undefined)}
+              >
+                Home
+                <span></span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="pokemons"
+                className={(nav) => (nav.isActive ? "nav-active" : undefined)}
+              >
+                Pokemons
+                <span></span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="contact"
+                className={(nav) => (nav.isActive ? "nav-active" : undefined)}
+              >
+                Contato
+                <span></span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
